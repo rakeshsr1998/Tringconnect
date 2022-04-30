31b55f65ba570be0f9b7726751a5b91611b4804f
Rakesh Tringapps <rakesh.sr@tringapps.com>
2022-05-01 00:26:19 +0530

    deleted workflow

--- src/Component/Layout/__test__/bottom-navigation.test.tsx
+++ src/Component/Layout/__test__/bottom-navigation.test.tsx
@@ -1,9 +1,8 @@
-import {fireEvent, render} from '@testing-library/react-native';
+import { fireEvent, render } from '@testing-library/react-native';
 import React from 'react';
-import {BottomNavigation} from '../BottomNavigation';
+import { BottomNavigation } from '../BottomNavigation';
 
 describe('<Header>', () => {
-  const mockFunction = jest.fn();
   beforeEach(() => {
     const component = <BottomNavigation />;
     instance = render(component);
@@ -16,31 +15,37 @@ describe('<Header>', () => {
 
   it('Should render header with home click', async () => {
     const nextButton = instance.getByTestId('home');
-    fireEvent(nextButton, 'onPress');
-    expect(mockFunction).toMatchSnapshot();
+    buttonClick(nextButton);
+
   });
 
   it('Should render header with call click', async () => {
     const nextButton = instance.getByTestId('cal');
-    fireEvent(nextButton, 'onPress');
-    expect(mockFunction).toMatchSnapshot();
+    buttonClick(nextButton);
+
   });
 
   it('Should render header with post click', async () => {
     const nextButton = instance.getByTestId('post');
-    fireEvent(nextButton, 'onPress');
-    expect(mockFunction).toMatchSnapshot();
+    buttonClick(nextButton);
+
   });
 
   it('Should render header with folder click', async () => {
     const nextButton = instance.getByTestId('folder');
-    fireEvent(nextButton, 'onPress');
-    expect(mockFunction).toMatchSnapshot();
+    buttonClick(nextButton);
+
   });
 
   it('Should render header with chat click', async () => {
     const nextButton = instance.getByTestId('chat');
-    fireEvent(nextButton, 'onPress');
-    expect(mockFunction).toMatchSnapshot();
+    buttonClick(nextButton);
   });
+
 });
+
+const buttonClick = (nextButton: any) => {
+  const mockFunction = jest.fn();
+  fireEvent(nextButton, 'onPress');
+  expect(mockFunction).toMatchSnapshot();
+}
