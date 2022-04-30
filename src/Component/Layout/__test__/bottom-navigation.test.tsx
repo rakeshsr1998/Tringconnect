de6353b7d86a86e7f23622c97efe5a20080c8959
Rakesh Tringapps <rakesh.sr@tringapps.com>
2022-05-01 00:28:54 +0530

    deleted workflow

--- src/Component/Layout/__test__/bottom-navigation.test.tsx
+++ src/Component/Layout/__test__/bottom-navigation.test.tsx
@@ -1,8 +1,9 @@
-import { fireEvent, render } from '@testing-library/react-native';
+import {fireEvent, render} from '@testing-library/react-native';
 import React from 'react';
-import { BottomNavigation } from '../BottomNavigation';
+import {BottomNavigation} from '../BottomNavigation';
 
 describe('<Header>', () => {
+  const mockFunction = jest.fn();
   beforeEach(() => {
     const component = <BottomNavigation />;
     instance = render(component);
@@ -15,37 +16,7 @@ describe('<Header>', () => {
 
   it('Should render header with home click', async () => {
     const nextButton = instance.getByTestId('home');
-    buttonClick(nextButton);
-
+    fireEvent(nextButton, 'onPress');
+    expect(mockFunction).toMatchSnapshot();
   });
-
-  it('Should render header with call click', async () => {
-    const nextButton = instance.getByTestId('cal');
-    buttonClick(nextButton);
-
-  });
-
-  it('Should render header with post click', async () => {
-    const nextButton = instance.getByTestId('post');
-    buttonClick(nextButton);
-
-  });
-
-  it('Should render header with folder click', async () => {
-    const nextButton = instance.getByTestId('folder');
-    buttonClick(nextButton);
-
-  });
-
-  it('Should render header with chat click', async () => {
-    const nextButton = instance.getByTestId('chat');
-    buttonClick(nextButton);
-  });
-
 });
-
-const buttonClick = (nextButton: any) => {
-  const mockFunction = jest.fn();
-  fireEvent(nextButton, 'onPress');
-  expect(mockFunction).toMatchSnapshot();
-}
